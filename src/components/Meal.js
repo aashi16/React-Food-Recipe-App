7e64f857fd445df613573ec9aee70dcf6230bed8
Aashi singh <83874139+aashi16@users.noreply.github.com>
2022-06-16 11:28:40 +0530

    ToggleState fixed

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 const Meal = (props) => {
-  const { items } = props;
+  const {
+    idMeal,
+    strMeal,
+    strMealThumb,
+    strInstructions,
+    strArea,
+    strCategory,
+  } = props;
   const [toggle, setToggle] = useState(false);
   const [icon, setIcon] = useState("+");
   function toggleBtn() {
@@ -9,34 +16,21 @@ const Meal = (props) => {
   }
   return (
     <div className="section-center">
-      {items.map((item) => {
-        const {
-          idMeal,
-          strMeal,
-          strMealThumb,
-          strInstructions,
-          strArea,
-          strCategory,
-        } = item;
-
-        return (
-          <article className="menu-item" key={idMeal}>
-            <img src={strMealThumb} alt={strMeal} className="photo" />
-            <div className="item-info">
-              <header>
-                <h3>{strMeal}</h3>
-                <h4 className="area">
-                  {strArea} ({strCategory})
-                </h4>
-              </header>
-              <button className="btn" onClick={toggleBtn}>
-                {icon}
-              </button>
-            </div>
-            {toggle && <p className="item-text">{strInstructions}</p>}
-          </article>
-        );
-      })}
+      <article className="menu-item" id={idMeal}>
+        <img src={strMealThumb} alt={strMeal} className="photo" />
+        <div className="item-info">
+          <header>
+            <h3>{strMeal}</h3>
+            <h4 className="area">
+              {strArea} ({strCategory})
+            </h4>
+          </header>
+          <button className="btn" onClick={toggleBtn}>
+            {icon}
+          </button>
+        </div>
+        {toggle && <p className="item-text">{strInstructions}</p>}
+      </article>
     </div>
   );
 };
